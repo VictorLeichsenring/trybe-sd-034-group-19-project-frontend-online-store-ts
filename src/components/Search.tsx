623f09991362b01deedb19bfcbbc09398b0384c0
keli <kr.menezes@yahoo.com>
2023-09-17 10:36:01 -0300

    iniciando o projeto

---
+++ src/components/Search.tsx
@@ -0,0 +1,14 @@
+export default function Search() {
+  return (
+    <div>
+      <input
+        type="text"
+        name=""
+        id="pesquisa"
+      />
+      <h2 data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </h2>
+    </div>
+  );
+}
