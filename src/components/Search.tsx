95079bd108257c6b9551e6261b77e278d497d37f
Larissa Kerchner <kerchnerchagaslarissa@gmail.com>
2023-09-17 17:46:57 -0300

    fix: elint

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import { getCategories } from '../services/api';
+import React, { useState } from 'react';
 import Categories from './Categories';
 
 export default function Search() {
