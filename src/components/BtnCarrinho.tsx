6d0b741e1a4da89685227ed3d97d5d6c7e37bab9
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-09-19 16:52:16 -0300

    Corrigido erros de atualização e logica de adicionar produtos ao carrinho

--- src/components/BtnCarrinho.tsx
+++ src/components/BtnCarrinho.tsx
@@ -2,18 +2,29 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function BtnCarrinho({ product }: any) {
-  const [addProducts, setAddProducts] = useState([]);
+  const [addProducts, setAddProducts] = useState<any[]>([]);
 
   const navigate = useNavigate();
 
   function AddCard() {
-    const updatedCart = [...addProducts, product];
-    setAddProducts(updatedCart as any);
+    // const updatedCart = [...addProducts, product];
+    // setAddProducts(updatedCart as any);
 
-    // adicionando no local storage
+    // // adicionando no local storage
+    // localStorage.setItem('addProducts', JSON.stringify(updatedCart));
+
+    // navigate('/cart');
+    // Recuperar o carrinho atual do localStorage
+    const currentCart = JSON.parse(localStorage.getItem('addProducts') || '[]');
+
+    // Adicionar o novo produto ao carrinho
+    const updatedCart = [...currentCart, product];
+
+    // Salvar o carrinho atualizado no localStorage
     localStorage.setItem('addProducts', JSON.stringify(updatedCart));
 
-    navigate('/cart');
+    // Atualizar o estado local (opcional, já que estamos usando o localStorage como fonte única de verdade)
+    setAddProducts(updatedCart);
   }
 
   // pegando do logal storage ao regarregar
