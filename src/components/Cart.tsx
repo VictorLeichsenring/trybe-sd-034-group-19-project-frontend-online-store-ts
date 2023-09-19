1d6f3bf89d9ad17104ae2dfb6f3bc3facbe48f66
Larissa Kerchner <kerchnerchagaslarissa@gmail.com>
2023-09-19 16:10:33 -0300

    requisito 10

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -4,6 +4,27 @@ import { Product } from './types';
 
 export default function Cart() {
   const [cartProducts, setCartProducts] = useState<Product[]>([]);
+  const [carrinho, setCarrinho] = useState([]);
+  const [quantidade, setQuantidade] = useState(1);
+
+  // Função para incrementar
+  function increment() {
+    setQuantidade(quantidade + 1);
+  }
+
+  // Função para decrementar
+  function decrement() {
+    if (quantidade > 1) {
+      setQuantidade(quantidade - 1);
+    }
+  }
+
+  // Função para remover produtos
+  function handleRemove(index: number) {
+    const novosServicos = [...cartProducts];
+    novosServicos.splice(index, 1);
+    setCartProducts(novosServicos);
+  }
 
   // Pegar os produtos do localStorage ao carregar a página
   useEffect(() => {
@@ -12,6 +33,11 @@ export default function Cart() {
       setCartProducts(JSON.parse(savedCart));
     }
   }, []);
+
+  useEffect(() => {
+    // Atualizar o localStorage sempre que o carrinho for modificado
+    localStorage.setItem('addProducts', JSON.stringify(cartProducts));
+  }, [cartProducts]);
 
   return (
     <div>
@@ -25,18 +51,36 @@ export default function Cart() {
         <ul>
           {cartProducts.map((product, index) => (
             <li key={ index }>
-              <button data-testid="remove-product">X</button>
               <p data-testid="shopping-cart-product-name">{product.title}</p>
               <p>
                 Preço: R$
                 {' '}
                 {product.price.toFixed(2)}
               </p>
-              <button data-testid="product-decrease-quantity">-</button>
+              <button
+                data-testid="product-decrease-quantity"
+                onClick={ decrement }
+              >
+                -
+
+              </button>
               <p data-testid="shopping-cart-product-quantity">
-                1
+                {quantidade}
               </p>
-              <button data-testid="product-increase-quantity">+</button>
+              <button
+                data-testid="product-increase-quantity"
+                onClick={ increment }
+              >
+                +
+
+              </button>
+              <button
+                data-testid="remove-product"
+                onClick={ () => handleRemove(index) }
+              >
+                X
+
+              </button>
             </li>
           ))}
         </ul>
